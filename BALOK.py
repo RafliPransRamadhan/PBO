9c0cfaa7f9c6227cc1236d803c8d9f43d36ef0bf
RafliPransRamadhan <148331069+RafliPransRamadhan@users.noreply.github.com>
2023-10-23 19:27:55 +0700

    Add files via upload

---
+++ BALOK.py
@@ -0,0 +1,16 @@
+# Menghitung luas permukaan dan volume balok
+
+# Panjang, lebar, dan tinggi balok
+panjang = 12
+lebar = 5
+tinggi = 7
+
+# Menghitung luas permukaan balok
+luas_permukaan = 2 * ((panjang * lebar) + (panjang * tinggi) + (lebar * tinggi))
+
+# Menghitung volume balok
+volume = panjang * lebar * tinggi
+
+# Menampilkan hasil
+print("Luas Permukaan Balok: ", luas_permukaan)
+print("Volume Balok: ", volume)
