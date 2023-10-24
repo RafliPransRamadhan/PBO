6c3a99a12f7e280962d163adde05c50dfc6ba798
RafliPransRamadhan <148331069+RafliPransRamadhan@users.noreply.github.com>
2023-10-24 18:03:50 +0700

    Add files via upload

---
+++ Pertemuan 1/PERSEGI PANJANG/PERSEGI PANJANG.py
@@ -0,0 +1,15 @@
+print("Menghitung Luas dan Keliling Persegi Panjang")
+
+# variabel
+panjang = 25
+lebar = 10
+
+# rumus
+luas = panjang * lebar
+keliling = (2 * panjang) + (2 * lebar)
+
+# output
+print("Panjang : ", panjang)
+print("Lebar : ", lebar)
+print("Luas : ", luas)
+print("Keliling : ", keliling) 
